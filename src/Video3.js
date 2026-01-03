38b3faada04c0b4fbfca46b5a8664b9e3abe0f33
PraetzGit <praettsdeutsch@gmail.com>
2026-01-03 17:35:57 +0100

    Update Video3.js

--- src/Video3.js
+++ src/Video3.js
@@ -1,24 +1,34 @@
 export default function Video3() {
-    return (
-      <div style={{ padding: "2rem", maxWidth: 900, margin: "auto", fontFamily: "Roboto, sans-serif" }}>
-        <h1 style={{ fontSize: "2rem", fontWeight: 700, color: "#333" }}>Simulation 3: Mit zusätzlicher Straße, ein Paradoxon </h1>
-        <p style={{ fontSize: "1.1rem", color: "#555" }}>
-          Hier wird das Verkehrsgeschehen mit zusätzlicher Straße gezeigt, dabei entsteht ein Paradoxon.
-        </p>
-  
-        <div style={{
+  return (
+    <div
+      style={{
+        padding: "2rem",
+        maxWidth: 900,
+        margin: "auto",
+        fontFamily: "Roboto, sans-serif",
+      }}
+    >
+      <h1 style={{ fontSize: "2rem", fontWeight: 700, color: "#333" }}>
+        3. Simulation in SUMO – Braess-Paradoxon
+      </h1>
+
+      <p style={{ fontSize: "1.1rem", color: "#555" }}>
+        Mehr als die Hälfte der Fahrzeuge nutzt die neue Straße. Dadurch erhöht
+        sich die durchschnittliche Reisezeit.
+      </p>
+
+      <video
+        controls
+        width="100%"
+        style={{
           marginTop: "2rem",
-          background: "#eee",
-          height: "500px",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          color: "#999",
-          fontSize: "1.5rem"
-        }}>
-          Video kommt hier hin
-        </div>
-      </div>
-    );
-  }
-  
+          borderRadius: "10px",
+          boxShadow: "0 4px 12px rgba(0,0,0,0.2)",
+        }}
+      >
+        <source src="/Paint1.mp4" type="video/mp4" />
+        Dein Browser unterstützt dieses Video nicht.
+      </video>
+    </div>
+  );
+}  
