306ec1e2f3dad148039fa0643c2b19381035be3e
PraetzGit <praettsdeutsch@gmail.com>
2026-01-03 17:35:32 +0100

    Update Video2.js

--- src/Video2.js
+++ src/Video2.js
@@ -9,27 +9,26 @@ export default function Video2() {
       }}
     >
       <h1 style={{ fontSize: "2rem", fontWeight: 700, color: "#333" }}>
-        Simulation 2: Mit zusätzlicher Straße, kein Paradoxon{" "}
+        2. Simulation in SUMO – Zusätzliche Straße, kein Paradoxon
       </h1>
+
       <p style={{ fontSize: "1.1rem", color: "#555" }}>
-        Hier wird das Verkehrsgeschehen mit zusätzlicher Straße gezeigt, dabei
-        entsteht aber kein Paradoxon.
+        Die zusätzliche Straße ist vorhanden, wird jedoch von weniger als der
+        Hälfte der Fahrzeuge genutzt.
       </p>
 
-      <div
+      <video
+        controls
+        width="100%"
         style={{
           marginTop: "2rem",
-          background: "#eee",
-          height: "500px",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          color: "#999",
-          fontSize: "1.5rem",
+          borderRadius: "10px",
+          boxShadow: "0 4px 12px rgba(0,0,0,0.2)",
         }}
       >
-        Video kommt hier hin
-      </div>
+        <source src="/Paint1.mp4" type="video/mp4" />
+        Dein Browser unterstützt dieses Video nicht.
+      </video>
     </div>
   );
 }
